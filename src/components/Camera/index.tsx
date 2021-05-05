6c23c61f4b4d2d5229fa6a70e7cc139655f716e9
João <vi.peres@icloud.com>
2021-05-05 14:55:13 -0300

    remove face-api and add tracking js

--- src/components/Camera/index.tsx
+++ src/components/Camera/index.tsx
@@ -1,10 +1,6 @@
 import { FC, useEffect, useRef } from 'react';
 
-import * as faceApi from 'face-api.js';
-
 import { Canvas, Container, FaceBox, Video } from './styles';
-
-const FACE_API_MODELS_PATH = '/face-api/models';
 
 const Camera: FC = () => {
   const videoRef = useRef<HTMLVideoElement>(null);
@@ -36,54 +32,17 @@ const Camera: FC = () => {
       }
     };
 
-    Promise.allSettled([
-      faceApi.nets.tinyFaceDetector.loadFromUri(FACE_API_MODELS_PATH),
-    ]).then(getWebcam);
+    getWebcam();
   }, [videoRef]);
 
   const handleOnPlay = async () => {
-    if (!videoRef.current || !canvasRef.current) {
-      return;
-    }
-
-    console.log('PLAY');
-
-    const canvas = faceApi.createCanvasFromMedia(videoRef.current);
-
-    const canvasSize = {
-      width: videoRef.current.videoWidth,
-      height: videoRef.current.videoHeight,
-    };
-
-    faceApi.matchDimensions(canvas, canvasSize);
-
-    canvasRef.current.innerHTML = '';
-    canvasRef.current.appendChild(canvas);
-
-    setInterval(async () => {
-      if (!videoRef.current) {
-        return console.log('Input video not working');
-      }
-
-      const detections = await faceApi.detectAllFaces(
-        videoRef.current,
-        new faceApi.TinyFaceDetectorOptions()
-      );
-
-      const resizedDetections = faceApi.resizeResults(detections, canvasSize);
-
-      faceApi.draw.drawDetections(canvas, resizedDetections);
-
-      setTimeout(() => {
-        canvas.getContext('2d')?.clearRect(0, 0, canvas.width, canvas.height);
-      }, 9900);
-    }, 10000);
+    console.log('Play');
   };
 
   return (
     <Container>
       <Canvas ref={canvasRef} />
-      <Video ref={videoRef} autoPlay muted />
+      <Video ref={videoRef} autoPlay muted onPlay={handleOnPlay} />
     </Container>
   );
 };
